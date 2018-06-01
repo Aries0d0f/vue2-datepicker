d86926caed0c6df65b7e8f92108b7383648a42d4
Aries Cs <aries0d0f@gmail.com>
2018-06-02 03:39:21 +0800

    Add Hightlight.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue              from 'vue'
 import FontAwesomeIcon  from '@fortawesome/vue-fontawesome'
 import fontawesome      from '@fortawesome/fontawesome'
 import solid            from '@fortawesome/fontawesome-free-solid'
-import vClickOutside from 'v-click-outside'
+import vClickOutside    from 'v-click-outside'
+import VueHighlightJS   from 'vue-highlightjs'
 
 import App              from './App'
 
@@ -12,6 +13,9 @@ Vue.component(FontAwesomeIcon.name, FontAwesomeIcon)
 
 // Add outside click directive
 Vue.use(vClickOutside)
+
+// Add Hightlight.js
+Vue.use(VueHighlightJS)
 
 const app = new Vue({
   ...App

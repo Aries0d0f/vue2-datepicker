b1c548c5597375090d455a8cb76ae8f4ea39d033
Aries Cs <aries0d0f@gmail.com>
2018-06-02 03:39:36 +0800

    Update demo page.

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,28 @@
     </div>
     <div class="container">
       <h2>Single date selection</h2>
-      <pre></pre>
-      <date-picker :singleDateSelection="true" v-model="date" />
+      <span v-highlightjs="sampleCode[0]"><code class="javascript"></code></span>
+      <date-picker :singleDateSelection="true" v-model="date.single" />
     </div>
     <div class="container">
-      <h2>Date range selection</h2>
-      <pre></pre>
-      <date-picker v-model="date" />
+      <h2>Date range selection (default)</h2>
+      <span v-highlightjs="sampleCode[1]"><code class="javascript"></code></span>
+      <date-picker v-model="date.default" />
+    </div>
+    <div class="container">
+      <h2>Custom format</h2>
+      <span v-highlightjs="sampleCode[2]"><code class="javascript"></code></span>
+      <date-picker :format="`YYYY/MM/DD`" v-model="date.customFormat" />
+    </div>
+    <div class="container">
+      <h2>Custom start date</h2>
+      <span v-highlightjs="sampleCode[3]"><code class="javascript"></code></span>
+      <date-picker :startDate="new Date(`2018-07-29`)" v-model="date.customFormat" />
+    </div>
+    <div class="container">
+      <h2>Custom end date</h2>
+      <span v-highlightjs="sampleCode[4]"><code class="javascript"></code></span>
+      <date-picker :endDate="new Date(`2018-07-29`)" v-model="date.customFormat" />
     </div>
   </div>
 </template>
@@ -26,7 +41,18 @@ export default {
   },
   data () {
     return {
-      date: null
+      date: {
+        single: null,
+        default: null,
+        customFormat: null
+      },
+      sampleCode: [
+        `<date-picker :singleDateSelection="true" v-model="date" />`,
+        `<date-picker v-model="date" />`,
+        `<date-picker :format="\`YYYY/MM/DD\`" v-model="date" />`,
+        `<date-picker :startDate="new Date(\`2018-07-29\`)" v-model="date" />`,
+        `<date-picker :endDate="new Date(\`2018-07-29\`)" v-model="date" />`
+      ]
     }
   }
 }

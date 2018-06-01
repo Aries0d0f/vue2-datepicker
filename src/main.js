ea8d297146db30c193f9b8dc562340d1f6161b8f
Aries Cs <aries0d0f@gmail.com>
2018-06-02 01:45:03 +0800

    Add package.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import Vue              from 'vue'
 import FontAwesomeIcon  from '@fortawesome/vue-fontawesome'
 import fontawesome      from '@fortawesome/fontawesome'
 import solid            from '@fortawesome/fontawesome-free-solid'
+import vClickOutside from 'v-click-outside'
 
 import App              from './App'
 
 // Add fontawesome icon
 fontawesome.library.add(solid)
 Vue.component(FontAwesomeIcon.name, FontAwesomeIcon)
+
+// Add outside click directive
+Vue.use(vClickOutside)
 
 const app = new Vue({
   ...App

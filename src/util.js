4d6c97326f60a538aeb71cea203c90debbde728e
Aries Cs <aries0d0f@gmail.com>
2018-06-02 01:46:42 +0800

    Modify calender functions.

--- src/util.js
+++ src/util.js
@@ -1,16 +1,20 @@
 import fecha from 'fecha'
 
+class Calender {
+  constructor (date) {
+    this.year = new Date(date).getFullYear()
+    this.month = new Date(date).getMonth()
+    this.daysArray = [...Array(new Date(this.year, this.month + 1, 0).getDate()).keys()].map(e => (new Date(this.year, this.month, e + 1)))
+    this.startWeekIndex = new Date(this.year, this.month, 1).getDay()
+  }
+}
+
 export default {
   getCalender: (month) => {
-    class Calender {
-      constructor (month) {
-        this.year = new Date(month).getFullYear()
-        this.month = new Date(month).getMonth() + 1
-        this.daysArray = [...Array(new Date(this.year, this.month, 0).getDate()).keys()].map(e => (new Date(this.year, this.month, e + 1)))
-        this.startWeekIndex = new Date(this.year, this.month - 1, 1).getDay()
-      }
-    }
     return new Calender(month)
+  },
+  getNextMonth: (month) => {
+  //
   },
   formatDate: (date, format) => {
     return date ? fecha.format(new Date(date), format) : null

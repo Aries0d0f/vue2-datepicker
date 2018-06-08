d2070912221a4bd24407efcabbacf7977cbbd14e
Aries Cs <aries0d0f@gmail.com>
2018-06-08 22:03:41 +0800

    Fix css not include in js.

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -3,10 +3,6 @@ const path = require('path')
 const utils = require('./utils')
 const webpack = require('webpack')
 const config = require('../config')
-const merge = require('webpack-merge')
-const baseWebpackConfig = require('./webpack.base.conf')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
@@ -14,21 +10,82 @@ const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
   : require('../config/prod.env')
 
-const webpackConfig = merge(baseWebpackConfig, {
-  entry: path.resolve(__dirname, '../src/components/DatePicker.vue'),
-  module: {
-    rules: utils.styleLoaders({
-      sourceMap: config.build.productionSourceMap,
-      extract: true,
-      usePostCSS: true
-    })
+const webpackConfig = {
+  resolve: {
+    extensions: ['.json', '.js', '.vue'],
+    alias: {
+      components: path.resolve(__dirname, '../src/components'),
+      '@': path.resolve(__dirname, '../src/'),
+      'vue$': 'vue/dist/vue.esm.js',
+    },
   },
+  entry: path.resolve(__dirname, '../src/components/DatePicker.vue'),
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
     path: config.build.assetsRoot,
     library: 'vue-datepicker',
     libraryTarget: 'umd',
     filename: 'vue-datepicker.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+            preserveWhitespace: false,
+          postcss: [
+            require('autoprefixer')({
+              browsers: ['last 10 versions', 'ie 11']
+            }),
+            require('cssnano')({
+              discardComments: { removeAll: true },
+            }),
+          ],
+
+          loaders: {
+            scss: 'vue-style-loader!css-loader!sass-loader?minimize?{discardComments:{removeAll:true}}',
+          },
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: {
+            loader: 'css-loader',
+            options: {
+              minimize: true
+            }
+          }
+        }),
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: 'images/[name].[hash:7].[ext]',
+          },
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: 'fonts/[name].[hash:7].[ext]',
+          },
+        },
+      }
+    ]
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
@@ -50,6 +107,6 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     new webpack.optimize.OccurrenceOrderPlugin()
   ]
-})
+}
 
 module.exports = webpackConfig
